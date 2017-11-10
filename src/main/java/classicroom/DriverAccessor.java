8732427b39fdc333da078d1413c0d304f9ea3d7f
dasawa <core.inside@outlook.jp>
2017-11-10 14:01:26 +0900

    H2接続

---
+++ src/main/java/classicroom/DriverAccessor.java
@@ -0,0 +1,46 @@
+package utility;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DriverAccessor {
+
+	private final static String DRIVER_URL = "jdbc:h2:./test";
+	private final static String DRIVER_NAME = "org.h2.Driver";
+	private final static String USER_NAME = "sa";
+	private final static String PASSWORD = "";
+
+	public Connection createConnection() {
+		try {
+			Class.forName(DRIVER_NAME);
+	        Connection con=DriverManager.getConnection(DRIVER_URL,USER_NAME,PASSWORD);
+	        return con;
+	        }catch(ClassNotFoundException e){
+	            System.out.println("Can't Find H2 Driver.\n");
+	            }catch(SQLException e){
+	                 System.out.println("Connection Error.\n");
+	                }
+	                return null;
+
+
+		}
+
+	public void closeConnection(Connection con){
+        try{
+            con.close();
+        }catch(Exception ex){}
+    }
+}
+
+/*テンプレ
+Connection con = null;
+con = createConnection();
+
+String sql = "SQL文";
+
+PreparedStatement stmt = con.prepareStatement(sql);
+ResultSet rs = stmt.executeQuery();
+
+stmt.close();
+con = null;
+*/
