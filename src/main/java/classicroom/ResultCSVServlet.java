0c8c1a52bf7be7365bb0a7d4cc7935ac10cb1f3b
isii <isii1850@gmail.com>
2017-11-30 16:28:09 +0900

    サーブレット名がかぶっていたのを修正

--- src/main/java/classicroom/ResultCSVServlet.java
+++ src/main/java/classicroom/ResultCSVServlet.java
@@ -2,16 +2,16 @@ package classicroom;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-@WebServlet(name = "MyPageServlet", urlPatterns = { "my_results.csv" }, loadOnStartup = 1)
+@WebServlet(name = "ResultCSVServlet", urlPatterns = { "my_results.csv" }, loadOnStartup = 1)
 public class ResultCSVServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
@@ -29,6 +29,6 @@ public class ResultCSVServlet extends HttpServlet {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 }
