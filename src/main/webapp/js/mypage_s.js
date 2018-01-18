02b86b0f00866d0b1d7ca0754c8166c52dca16ea
isii <isii1850@gmail.com>
2018-01-18 10:50:13 +0900

    コメントアウトされていた旧スクリプト削除

--- src/main/webapp/js/mypage_s.js
+++ src/main/webapp/js/mypage_s.js
@@ -1,38 +1,3 @@
-/*
-var subjects;
-document.addEventListener('DOMContentLoaded', function() {
-    var subjectsReq = new XMLHttpRequest();
-    subjectsReq.onreadystatechange = function() {
-        if (subjectsReq.readyState == 4 && subjectsReq.status == 200) {
-            subjects = JSON.parse(subjectsReq.responseText);
-            var graphTab = document.getElementById('graph-tab');
-            var graphTabContents = document.getElementById('graph-tab-contents');
-            subjects.forEach(function(e) {
-                var button = document.createElement('button');
-                button.type = 'button';
-                button.classList.add('tab-buttons');
-                button.setAttribute('onclick', 'switchTab(this,\'subject-' + e.subject_id + '\')');
-                button.textContent = e.subject_name;
-                var tabContent = document.createElement('div');
-                tabContent.id = 'subject-' + e.subject_id;
-                tabContent.classList.add('tab-contents');
-                tabContent.innerHTML = '<svg id="result-graph-' + e.subject_id + '" class="result-graph"></svg>' +
-                    '<div class="result-info">' +
-                    '<p id="test_name-' + e.subject_id + '"></p>' +
-                    '<p id="my_point-' + e.subject_id + '"></p>' +
-                    '<p id="average_point-' + e.subject_id + '"></p>' +
-                    '</div>';
-                graphTab.appendChild(button);
-                graphTabContents.appendChild(tabContent);
-            });
-            document.getElementsByClassName('tab-buttons')[0].click();
-        }
-    };
-    subjectsReq.open('GET','api/subjects');
-    subjectsReq.send();
-});
-*/
-
 document.addEventListener('DOMContentLoaded', function() {
     makeResultGraphTabs();
 
