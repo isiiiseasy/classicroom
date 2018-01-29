8016a5f0c6c1a3baded854b95b6f73049be4b83e
isii <isii1850@gmail.com>
2018-01-29 10:32:15 +0900

    テストページの教師用、生徒用の分岐

--- src/main/java/classicroom/TestServlet.java
+++ src/main/java/classicroom/TestServlet.java
@@ -7,12 +7,24 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "TestServlet", urlPatterns = { "test" }, loadOnStartup = 1)
 public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/jsp/student/test.jsp").forward(request, response);
+		DataBase db = new DataBase();
+
+		HttpSession session = request.getSession(false);
+		Object userRank = request.getSession(false).getAttribute("userRank");
+		if (userRank.equals("student")) {
+			request.getRequestDispatcher("/WEB-INF/jsp/student/test.jsp").forward(request, response);
+		} else if (userRank.equals("teacher")) {
+			request.getRequestDispatcher("/WEB-INF/jsp/teacher/test.jsp").forward(request, response);
+		} else {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+		}
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
