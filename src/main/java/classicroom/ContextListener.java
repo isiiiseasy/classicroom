665d1d1cf73a06458e02846ddbbf8e94e0f7a3aa
isii <isii1850@gmail.com>
2018-01-30 14:48:07 +0900

    auto_incrementにしたことでサンプルデータが鯖再起のたびに増加するようになっていたのを防止

--- src/main/java/classicroom/ContextListener.java
+++ src/main/java/classicroom/ContextListener.java
@@ -1,5 +1,8 @@
 package classicroom;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -12,11 +15,15 @@ public class ContextListener implements ServletContextListener {
     }
 
     public void contextInitialized(ServletContextEvent ev) {
-    	DataBase db = new DataBase();
+		if (Files.exists(Paths.get("test.mv.db"))) { // DBファイルが存在したら何もせず返る
+			System.out.println("dbファイルが既にあるためテーブル作成・サンプルデータの挿入を行いません");
+		}else {
+			DataBase db = new DataBase();
 
-        db.createDataBase();	//テーブル作成
-        db.helloTableList();	//テーブル一覧表示
-        db.sampleDate();		//サンプルデータ挿入
-        db.print();				//行表示
+    		db.createDataBase();	//テーブル作成
+    		db.helloTableList();	//テーブル一覧表示
+    		db.sampleDate();		//サンプルデータ挿入
+    		db.print();				//行表示
+		}
     }
 }
