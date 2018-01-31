c0c2d2b17aad1a0c75656d616f51eb9dc103cb28
dasawa <core.inside@outlook.jp>
2018-01-31 14:46:06 +0900

    授業ファイル保存機能(仮)

--- src/main/java/classicroom/FileUploadServlet.java
+++ src/main/java/classicroom/FileUploadServlet.java
@@ -5,6 +5,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
@@ -24,43 +26,49 @@ public class FileUploadServlet extends HttpServlet {
   {
     // ファイルの保存
     DataBase db = new DataBase();
-    
+
     String flg = request.getParameter("flg");
 
 	if(flg.equals("0")) {	//アイコン画像保存
-		
+
 		File img = new File(this.getServletContext().getRealPath("/img"));
-	
+
 		HttpSession session = request.getSession(false);
-	
+
 		String userId = (String) session.getAttribute("userId");
 	    Part filePart = request.getPart("file");
 	    String fileName = (new StringBuilder(userId)
 	      .append("_img.jpg").toString());
-	
+
 	    save(filePart, new File(img, fileName));
-	
+
 	    db.IconSet(userId, fileName);
-	
+
 	    response.sendRedirect("mypage");
-	    
+
 	}else {					//授業ファイル保存
-		
-		File img = new File(this.getServletContext().getRealPath("/lessonfile"));
-	
+
 	    Part filePart = request.getPart("file");
 	    String fileName = extractFileName(filePart);
-	
-	    save(filePart, new File(img, fileName));
-	
-	    
-	
-	    response.sendRedirect("mypage");
+
+	    String lessonId = request.getParameter("lessonId");
+
+	    try {
+            Files.createDirectories(Paths.get("data/lessonfile/"+lessonId));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+		File lessonfile = new File("data/lessonfile/"+lessonId);
+
+	    save(filePart, new File(lessonfile, fileName));
+
+	    response.sendRedirect("lessonupload");
 	}
 
 
   }
-  
+
   private String extractFileName(Part part) { //ファイル名取得
 	  String[] splitedHeader = part.getHeader("Content-Disposition").split(";");
 
@@ -72,7 +80,7 @@ public class FileUploadServlet extends HttpServlet {
 	  }
 	  return fileName;
 	}
-  
+
   public void save(Part in, File out) throws IOException {
     BufferedInputStream br
       = new BufferedInputStream(in.getInputStream());
