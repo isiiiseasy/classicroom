185753d25eb2f9e4ed635503cb0395f10767656d
isii <isii1850@gmail.com>
2018-02-01 08:27:09 +0900

    生徒用テストページの回答の提出機能以外完成

--- src/main/webapp/WEB-INF/jsp/student/test.jsp
+++ src/main/webapp/WEB-INF/jsp/student/test.jsp
@@ -1,74 +1,31 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java"%>
-<%@ page import="java.sql.*" %>
+<%@ page import="java.sql.*"%>
 
 <!DOCTYPE html>
 <html>
-  <head>
-  	<link href="<%=request.getContextPath() %>/css/main.css" rel="stylesheet" type="text/css" media="screen" />
-    <title>テストページ画面</title>
-    <meta charset="utf-8">
-    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-  </head>
-  <body>
+<head>
+<link href="<%=request.getContextPath()%>/css/main.css"
+    rel="stylesheet" type="text/css" media="screen" />
+<title>テストページ画面</title>
+<meta charset="utf-8">
+<script src="<%=request.getContextPath()%>/js/test_s.js"
+    charset="utf-8"></script>
+</head>
+<body>
 
-  	<%@ include file="/WEB-INF/jsp/pageheader.jsp" %>
+    <%@ include file="/WEB-INF/jsp/pageheader.jsp"%>
 
     <div id="L_box">
-      <img src="">
-
-
+        <h2>テスト一覧</h2>
+        <ul id="test-list"></ul>
     </div>
+
     <div id="R_box">
+        <h1 id="midasi_1">テストページ</h1>
 
-    <h1 id="midasi_1">テストページ</h1>
-
-	<p>解答は一回しかできないので、見直しをしっかりおこないましょう！！！！</p>
-    <%--<HR style="margin: 3em 0 ;">--%>
-
-   	<div id="modal-content">
-   	 <form method="post" action="result">
-      <h3>問1.正しい表記はどれ？</h3>
-      <input type="radio" name="que-1" value="no1">1.Jaav
-      <input type="radio" name="que-1" value="no2">2.Java
-      <input type="radio" name="que-1" value="no3">3.Baka
-      <input type="radio" name="que-1" value="no4">4.jaxa
-
-      <h3>問2.整数型はどれ？</h3>
-      <input type="radio" name="que-2" value="no1">1.String
-      <input type="radio" name="que-2" value="no2">2.float
-      <input type="radio" name="que-2" value="no3">3.boolean
-      <input type="radio" name="que-2" value="no4">4.int
-
-      <h3>問3.コンストラクタを説明した文章として最も適したものを選びなさい。</h3>
-      1.処理内容の無いメソッドだけを定義し、実際の処理はインタフェースの実装したクラスで実装する。<br>
-      2.スーパークラスにおいて定義されているインスタンスメソッドを、 サブクラス内で再定義すること。<br>
-      3.クラスからインスタンスを生成する際に呼び出される部分。インスタンス生成時には必ず呼び出される。<br>
-      4.実行中のプログラムのメモリが不足しないように自動的に管理する仕組み。<br>
-      <input type="radio" name="que-3" value="no1">1
-      <input type="radio" name="que-3" value="no2">2
-      <input type="radio" name="que-3" value="no3">3
-      <input type="radio" name="que-3" value="no4">4
-
-      <h3>問4.あなたの今日の気分は？</h3>
-      <input type="radio" name="que-4" value="no1">1.寿司
-      <input type="radio" name="que-4" value="no2">2.カレー
-      <input type="radio" name="que-4" value="no3">3.ラーメン
-      <input type="radio" name="que-4" value="no4">4.ハンバーグ
-
-    <br><br>
-
-    <input type="submit" value="採点"><input type="reset" value="リセット">
-    </form>
-    <p><a id="modal-close" class="button-link">閉じる</a></p>
+        <h2 id="test-title">問題</h2>
+        <ol id="question-list"></ol>
+        <button id="submit-answer">回答を提出する</button>
     </div>
-
-    <%--<HR style="margin: 3em 0 ;">--%>
-
-    <p><a id="modal-open" class="button-link">テスト開始</a></p>
-    </div>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-	<script src="<%=request.getContextPath() %>/js/modal.js"></script>
-
-  </body>
+</body>
 </html>
---
+++ src/main/webapp/js/test_s.js
@@ -0,0 +1,115 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getSectionData();
+});
+
+function getSectionData() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var data = JSON.parse(xhr.responseText);
+            makeSubjectList(data);
+        }
+    };
+    xhr.open('GET', 'api/sections');
+    xhr.send();
+}
+
+function makeSubjectList(data) {
+    var testList = document.getElementById('test-list');
+    Array.prototype.slice.call(testList.children).forEach(function(child) {
+        child.parentNode.removeChild(child);
+    });
+    data.forEach(function(subject) {
+        var DOMSubject = document.createElement('li');
+        var DOMInnerList = document.createElement('ol');
+        DOMInnerList.id = 'test-list-' + subject.subject_id;
+        DOMSubject.textContent = subject.subject_name;
+
+        DOMSubject.appendChild(DOMInnerList);
+        testList.appendChild(DOMSubject);
+    });
+    getTestData();
+}
+
+function getTestData() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var data = JSON.parse(xhr.responseText);
+            makeTestList(data);
+        }
+    };
+    xhr.open('GET', 'api/tests');
+    xhr.send();
+}
+
+
+function makeTestList(data) {
+    data.forEach(function(test) {
+        var DOMInnerList = document.getElementById('test-list-' + test.subject_id) ;
+        var DOMTest = document.createElement('li');
+        DOMTest.addEventListener('click', getQuestions.bind(DOMTest, test.test_id));
+        DOMTest.textContent = test.test_name;
+
+        DOMInnerList.appendChild(DOMTest);
+    });
+}
+
+function makeQuestionList(data) {
+    var list = document.getElementById('question-list');
+    Array.prototype.slice.call(list.children).forEach(function(child) {
+        child.parentNode.removeChild(child);
+    });
+    for (var i = 0; i < data.length; i++) {
+        var li = document.createElement('li');
+        var q = document.createElement('p');
+        q.classList.add('question');
+        q.textContent = data[i].question;
+        li.appendChild(q);
+        li.appendChild(document.createElement('br'));
+        for (var j = 0; j < 4; j++) {
+            var c = document.createElement('label');
+            var radio = document.createElement('input');
+            radio.setAttribute('type', 'radio');
+            radio.classList.add('answer');
+            radio.name = 'answer' + i;
+            radio.value = j;
+            if (j == data[i].answer) {
+                radio.__correct__ = true;
+            }
+            c.appendChild(radio);
+            c.appendChild(document.createTextNode(data[i].choices[j]));
+            c.classList.add('choice');
+            li.appendChild(c);
+        }
+        list.appendChild(li);
+    }
+}
+
+
+function getQuestions(test_id) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            var data;
+            try {
+                data = JSON.parse(xhr.responseText);
+            } catch (e) {
+                data = [];
+            }
+            makeQuestionList(data);
+            document.getElementById('submit-answer').onclick = submitAnswer.bind(null, test_id);
+        }
+    };
+    xhr.open('GET', 'api/testcontents?test-id=' + test_id);
+    xhr.send();
+    document.getElementById('test-title').textContent = this.textContent;
+}
+
+function submitAnswer(test_id) {
+// 鯖側ができてから書く
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+});
