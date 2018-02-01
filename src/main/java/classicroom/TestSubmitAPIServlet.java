cd41f3f40969e079bc83900520fcda8225cfefb8
isii <isii1850@gmail.com>
2018-02-01 10:22:11 +0900

    サーブレット名かぶりとセッションパラメータ名間違いを修正

--- src/main/java/classicroom/TestSubmitAPIServlet.java
+++ src/main/java/classicroom/TestSubmitAPIServlet.java
@@ -8,12 +8,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "TestAPIServlet", urlPatterns = { "api/testsubmit" }, loadOnStartup = 1)
+@WebServlet(name = "TestSubmitAPIServlet", urlPatterns = { "api/testsubmit" }, loadOnStartup = 1)
 public class TestSubmitAPIServlet extends HttpServlet {
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-    	String userId = (String)request.getSession(false).getAttribute("user-id");
+    	String userId = (String)request.getSession(false).getAttribute("userId");
         String testId = request.getParameter("test-id");
         String point = request.getParameter("point");
 
